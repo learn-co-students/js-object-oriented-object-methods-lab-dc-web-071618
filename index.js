63b3ed789604afee82a54ee93cce815d3672de23
aubreeabril <aubree.abril@gmail.com>
2018-09-01 20:59:42 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  this.veto = function() {
+    return "No, I must disagree"
+  }
+  this.approve = () => {return "You can do that!"}
+  this.doCharity = () => {return "I like to help people."}
+  this.releasePressStatement = function() {
+    return 'You will see great things from Scuber.'
+  }
+  this.sayHi = function() {
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+  }
+}
